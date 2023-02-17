b82f74ede99075dee967d57b1df9129164236ed2
AlexTanghy <alexthy95@gmail.com>
2023-02-17 14:32:01 +0800

    0217 AT

--- src/Data/Case.js
+++ src/Data/Case.js
@@ -1,31 +1,11 @@
 import moment from "moment";
 const CaseItem = [
-<<<<<<< HEAD
   { id: 4, title: "K-77795MY-0 X 2 SET , 4500型楔焊机", caseno: "CAS-220805-01064", caseresource: "Resource - Unnmaed Resource", start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"), duration: 60, status: "Active" },
   { id: 5, title: "(WMDa04) 更換 HAN-71710670", caseno: "CAS-220805-01088", caseresource: "Resource - Unnmaed Resource", start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"), duration: 60, status: "Active" },
   { id: 6, title: "更換 HAN-71710670", caseno: "CAS-220805-01090", caseresource: "Resource - Unnmaed Resource", start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"), duration: 60, status: "Active" },
   { id: 7, title: "(WMDa04) 更換 HAN-71718670", caseno: "CAS-220805-01074", caseresource: "Resource - Unnmaed Resource", start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"), duration: 60, status: "Active" },
-=======
-  {
-    id: 4,
-    title: "K-77795MY-0 X 2 SET , 4500型楔焊机",
-    caseno: "CAS-220805-01064",
-    caseresource: "",
-    start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"),
-    duration: 60,
-    status: "Open",
-  },
-  {
-    id: 5,
-    title: "(WMDa04) 更換 HAN-71710670",
-    caseno: "CAS-220805-01088",
-    caseresource: "",
-    start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"),
-    duration: 60,
-    status: "Open",
-  },
-  { id: 6, title: "更換 HAN-71710670", caseno: "CAS-220805-01090", caseresource: "", start: moment().add(2, "hour").format("DD/MM/yyyy hh:mm:ss"), duration: 60, status: "Open" },
->>>>>>> a49a3bcce80a52eda30de5a1cf21e8fe7aa8ffa0
+
+  
 ];
 
 export default CaseItem;
