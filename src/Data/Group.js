1b7040d9b1a7bb662fdbb1b1a70ee6aa9c5a9cc6
AlexTanghy <alexthy95@gmail.com>
2023-02-22 16:16:51 +0800

    0222 AT

--- src/Data/Group.js
+++ src/Data/Group.js
@@ -1,9 +1,9 @@
 const tlGroups = [
-  { id: 1, img: "images/OF Otis Falls.jpg", content: "<B>Tom Mok - Tech Lead ", department: "(2h booked  -  5%)  </br> ", percent: 2 },
-  { id: 2, img: "images/JO Jim Olive.jpg", content: "<B>Helicon Ho - Senior Engineer", department: "(4h booked  - 8%)  </br>", percent: 10 },
-  { id: 3, img: "images/tomc.jpg", content: "<B>Ivan Lee - Senior Engineer", department: "(6h booked  - 15%) </br>", percent: 5 },
-  { id: 4, img: "images/LT Lina Townsend.jpg", content: "<B>Natalie Chan - Poject Manager", department: "(7h booked  - 17.5%) </br>", percent: 3 },
-  { id: 5, img: "images/EH Ester Henderson.jpg", content: "<B>McCree Li - Engineer", department: "(8h booked  - 20%)  </br>", percent: 4 },
+  { id: 1, img: "/images/OF Otis Falls.jpg", content: "<B>Tom Mok - Tech Lead ", department: "(2h booked  -  5%)  </br> ", percent: 2 },
+  { id: 2, img: "/images/JO Jim Olive.jpg", content: "<B>Helicon Ho - Senior Engineer", department: "(4h booked  - 8%)  </br>", percent: 10 },
+  { id: 3, img: "/images/tomc.jpg", content: "<B>Ivan Lee - Senior Engineer", department: "(6h booked  - 15%) </br>", percent: 5 },
+  { id: 4, img: "/images/LT Lina Townsend.jpg", content: "<B>Natalie Chan - Poject Manager", department: "(7h booked  - 17.5%) </br>", percent: 3 },
+  { id: 5, img: "/images/EH Ester Henderson.jpg", content: "<B>McCree Li - Engineer", department: "(8h booked  - 20%)  </br>", percent: 4 },
 ];
 
 export default tlGroups;
