b61ce31c91b21727a5236d593cd4e2a6dc5b617e
AlexTanghy <alexthy95@gmail.com>
2023-03-13 09:53:53 +0800

    0313 AT

--- src/Data/Item.js
+++ src/Data/Item.js
@@ -1,85 +1,85 @@
 import moment from "moment";
 const fakeBoardData = [
-  {
-    id: 1,
-    editable: true,
-    start: moment().add(2, "hour"),
-    end: moment().add(4, "hour"),
-    duration: moment().add(4, "hour").diff(moment().add(2, "hour")).toString(),   
-    content: "<a href='https://tstdrv2641016.app.netsuite.com/app/crm/support/supportcase.nl?id=1161&whence=' target ='blank' ><b>CAS-230222-01149 - AIRBUS </b></a> </br>"
-            +"20230222 清洗變速箱油底殼濾網- EPS-330T  </br>"
-            +"<I>#Low</I>",             //case
-    group: 1,
-    status: "Open",
-  },
-  {
-    id: 2,
-    content:  "<a href='https://tstdrv2641016.app.netsuite.com/app/crm/support/supportcase.nl?id=1164' target ='blank' ><b>CAS-230223-01151 - AIRBUS </b> </a></br>"
-            +"20230223 季度維修保養檢查 LB2000EXII(M)</br>"
-            +"<I>#Low</I>",
-    editable: true,
-    start: moment().add(7, "hour"),
-    end: moment().add(11, "hour"),
-    group: 2,
-    status: "Open",
-  },
-  {
-    id: 3,
-    content: "<b>CAS-230222-01451 - Zanlan Corp. </b> </br>"
-              +"維修保養 MB46VAE</br>"
-              +"<I>#Low</I>",
-    editable: true,
-    start: moment().add(4, "hour"),
-    end: moment().add(7, "hour"),
-    group: 3,
-    status: "Open",
-  },
+  // {
+  //   id: 1,
+  //   editable: true,
+  //   start: moment().add(2, "hour"),
+  //   end: moment().add(4, "hour"),
+  //   duration: moment().add(4, "hour").diff(moment().add(2, "hour")).toString(),   
+  //   content: "<a href='https://tstdrv2641016.app.netsuite.com/app/crm/support/supportcase.nl?id=1161&whence=' target ='blank' ><b>CAS-230222-01149 - AIRBUS </b></a> </br>"
+  //           +"20230222 清洗變速箱油底殼濾網- EPS-330T  </br>"
+  //           +"<I>#Low</I>",             //case
+  //   group: 1,
+  //   status: "Open",
+  // },
+  // {
+  //   id: 2,
+  //   content:  "<a href='https://tstdrv2641016.app.netsuite.com/app/crm/support/supportcase.nl?id=1164' target ='blank' ><b>CAS-230223-01151 - AIRBUS </b> </a></br>"
+  //           +"20230223 季度維修保養檢查 LB2000EXII(M)</br>"
+  //           +"<I>#Low</I>",
+  //   editable: true,
+  //   start: moment().add(7, "hour"),
+  //   end: moment().add(11, "hour"),
+  //   group: 2,
+  //   status: "Open",
+  // },
+  // {
+  //   id: 3,
+  //   content: "<b>CAS-230222-01451 - Zanlan Corp. </b> </br>"
+  //             +"維修保養 MB46VAE</br>"
+  //             +"<I>#Low</I>",
+  //   editable: true,
+  //   start: moment().add(4, "hour"),
+  //   end: moment().add(7, "hour"),
+  //   group: 3,
+  //   status: "Open",
+  // },
 
-  {
-    id: 4,
-    content: "<b>CAS-230223-01851 - Guildford Water Department </b> </br>"
-              +"維修保養 MB46VAE</br>"
-              +"<I>#High</I>",
-    editable: true,
-    start: moment().add(0, "hour"),
-    end: moment().add(8, "hour"),
-    group:5,
-    status: "In-Progress",
-  },
+  // {
+  //   id: 4,
+  //   content: "<b>CAS-230223-01851 - Guildford Water Department </b> </br>"
+  //             +"維修保養 MB46VAE</br>"
+  //             +"<I>#High</I>",
+  //   editable: true,
+  //   start: moment().add(0, "hour"),
+  //   end: moment().add(8, "hour"),
+  //   group:5,
+  //   status: "In-Progress",
+  // },
 
-  {
-    id: 5,
-    content: "<b>CAS-230223-08981 - The Device Shop</b></br>"
-              +"20230222 清洗變速箱油底殼濾網 - EPS-330T</br>"
-              +"<I>#Medium</I>",
-    editable: true,
-    start: moment().add(-2, "hour"),
-    end: moment().add(1, "hour"),
-    group:3,
-    status: "Complete",
-  },
-  {
-    id: 6,
-    content: "<b>CAS-230223-05951 - Zuni Home Crafts Ltd.</b></br>"
-              +"(WMDa04) 更換 HAN-71718670 </br>"
-              +"<I>#Medium</I>",
-    editable: true,
-    start: moment().add(-1, "hour"),
-    end: moment().add(6, "hour"),
-    group:4,
-    status: "Complete",
-  },
-  {
-    id: 7,
-    content: "<b>CAS-230805-01018 - Zuni Home Crafts Ltd. </b></br>"
-              +"(WMDa04) 更換 HAN-71718670 </br>"
-              +"<I>#Low</I>",
-    editable: true,
-    start: moment().add(1, "hour"),
-    end: moment().add(4, "hour"),
-    group:3,
-    status: "Open",
-  },
+  // {
+  //   id: 5,
+  //   content: "<b>CAS-230223-08981 - The Device Shop</b></br>"
+  //             +"20230222 清洗變速箱油底殼濾網 - EPS-330T</br>"
+  //             +"<I>#Medium</I>",
+  //   editable: true,
+  //   start: moment().add(-2, "hour"),
+  //   end: moment().add(1, "hour"),
+  //   group:3,
+  //   status: "Complete",
+  // },
+  // {
+  //   id: 6,
+  //   content: "<b>CAS-230223-05951 - Zuni Home Crafts Ltd.</b></br>"
+  //             +"(WMDa04) 更換 HAN-71718670 </br>"
+  //             +"<I>#Medium</I>",
+  //   editable: true,
+  //   start: moment().add(-1, "hour"),
+  //   end: moment().add(6, "hour"),
+  //   group:4,
+  //   status: "Complete",
+  // },
+  // {
+  //   id: 7,
+  //   content: "<b>CAS-230805-01018 - Zuni Home Crafts Ltd. </b></br>"
+  //             +"(WMDa04) 更換 HAN-71718670 </br>"
+  //             +"<I>#Low</I>",
+  //   editable: true,
+  //   start: moment().add(1, "hour"),
+  //   end: moment().add(4, "hour"),
+  //   group:3,
+  //   status: "Open",
+  // },
 ];
 
 export default fakeBoardData;
